7d914bc3d779ea219f520b8c863cb48e59d8e23a
Micheal Giles <mikegg21@gmail.com>
2018-09-15 00:38:58 -0500

    login now keeps the users state in localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -10,6 +14,16 @@ import Register from './components/auth/register';
 import Login from './components/auth/login';
 
 import './App.css';
+
+//check for jwtToken
+if(localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   constructor(props) {
