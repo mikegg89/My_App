61768c6de62d3018a1a752f85f1ac54d76961156
Micheal Giles <mikegg21@gmail.com>
2018-09-15 02:18:15 -0500

    new Branding

--- client/src/components/layout/navBar/navBar.jsx
+++ client/src/components/layout/navBar/navBar.jsx
@@ -48,7 +48,7 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <Link className="navbar-brand" to="/">My App</Link>
+          <Link className="navbar-brand" to="/">Story Time</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon"></span>
           </button>
